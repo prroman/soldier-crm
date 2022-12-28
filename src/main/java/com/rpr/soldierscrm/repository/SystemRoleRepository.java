c7ee857671446bb0254b0e3a20ad8a1c4ac1b101
root <toor>
2022-12-28 13:46:09 +0100

    Revert "Security JWT initial version"
    This reverts commit 5ed97b06

--- src/main/java/com/rpr/soldierscrm/repository/SystemRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rpr.soldierscrm.repository;
-
-import com.rpr.soldierscrm.entity.SystemRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SystemRoleRepository extends JpaRepository<SystemRole, Long> {
-    SystemRole findByName(String name);
-}
